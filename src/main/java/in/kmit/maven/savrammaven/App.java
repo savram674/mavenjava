aa65e224805d5d3890a6baaaed029e70825dd7a3
savram674 <savram674@gmail.com>
2022-08-17 12:56:17 +0530

    maven java

---
+++ src/main/java/in/kmit/maven/savrammaven/App.java
@@ -0,0 +1,13 @@
+package in.kmit.maven.savrammaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to Savitha's WWorld!" );
+    }
+}
